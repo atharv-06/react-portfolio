ce4f20345a0804e57e65a64824d47f98074f77d4
Hi I'm Atharv <147968786+atharv-06@users.noreply.github.com>
2026-01-03 14:06:36 +0530

    Delete react-portfolio directory

--- react-portfolio/src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="mt-10 py-6 text-center text-sm text-slate-500">
-      © 2026 Atharv Lokhande • React Portfolio
-    </footer>
-  );
-};
-
-export default Footer;
